ce614cbd79947d9b2c3fb7c29eefb03138f894c7
towhid046 <towhidmorol46@gmail.com>
2024-04-27 15:55:04 +0600

    fix the home craft card design

--- src/components/Unique/CraftItem/CraftItem.jsx
+++ src/components/Unique/CraftItem/CraftItem.jsx
@@ -1,7 +1,12 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
-
+import { useContext } from "react";
+import { UserContext } from "../../../providers/ContextProvider/ContextProvider";
+import { CiStar } from "react-icons/ci";
+import { BiDollar } from "react-icons/bi";
 const CraftItem = ({ craft }) => {
+  const { mode } = useContext(UserContext);
+
   const {
     _id,
     user_name,
@@ -17,22 +22,49 @@ const CraftItem = ({ craft }) => {
     price,
   } = craft;
   return (
-    <div className="card card-compact bg-base-100 border">
-      <figure>
-        <img className="w-full" src={photoUrl} alt="Craft Img" />
+    <div className="bg-base-100 shadow-lg flex flex-col justify-between">
+      <figure className="relative cursor-pointer overflow-hidden">
+        <Link to={`/craft-details/${_id}`}>
+          <img
+            className="w-full h-64 hover:scale-125 transition ease-in-out duration-500"
+            src={photoUrl}
+            alt="Craft Img"
+          />
+        </Link>
+        <span className="absolute top-3 right-3 btn rounded-none  btn-sm">
+          {stock_status}
+        </span>
       </figure>
-      <div className="card-body">
-        <h2 className="card-title">{item_name}</h2>
-        <p>{short_description}</p>
-        <p>Rating: {rating}</p>
-        <p>Processing Time: {processing_time}</p>
-        <p>Price: {price}</p>
-        <strong>Stock: {stock_status}</strong>
-        <div className="card-actions justify-end">
-          <Link className="w-full" to={`/craft-details/${_id}`}>
-            <button className="btn w-full btn-primary">View Details</button>
-          </Link>
+      <div className="p-4">
+        <div className="mb-4 space-y-1">
+          <h2 className="text-2xl font-semibold mb-2">{item_name}</h2>
+          <p className="pb-2">
+            <em>{subcategory_name}</em>
+          </p>
+          <hr />
+          <p className="pt-2">
+            {short_description.split(" ").splice(0, 12).join(" ")} . . .
+          </p>
+
+          <div className="py-1 flex items-center text-xl text-[#e7a261]">
+            <BiDollar className="text-2xl text-" />
+            <strong>{price}</strong>
+          </div>
+          <p>
+            <strong className="">Rating:</strong> {rating}
+          </p>
+          <p>Processing Time: {processing_time} Days</p>
         </div>
+
+        <Link className="w-full" to={`/craft-details/${_id}`}>
+          <button
+            className={`btn w-full rounded-none ${
+              mode ? "bg-[#AB7442]" : "bg-gray-800"
+            } border-none text-gray-50 hover:bg-gray-900 `}
+          >
+            View Details
+          </button>
+        </Link>
       </div>
     </div>
   );
